6b64a23a205b5d53a47e35a19fb2022874d83af9
igorNeves007 <igorneves0404@gmail.com>
2022-11-10 21:13:47 -0300

    Fixing Routes

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ import { CreateUserController } from './controllers/User/CreateUserController';
 
 const router = Router();
 
-router.post('/users', new CreateUserController().handle)
+router.post('/cadastro', new CreateUserController().handle)
 router.post('/login', new AuthController().handle)
 
 export { router }
