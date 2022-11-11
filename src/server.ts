0c431f5fdc0124c6270612195ae261f54bc8260a
myguelangello <myguelangello624@gmail.com>
2022-11-10 23:00:31 -0300

    add dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+
 import "reflect-metadata";
 import express from 'express';
 import cors from 'cors';
